3d9ad782aeac81f888e1876c23fcd4e292a090a2
jiemaoxue <jiemx2008@163.com>
2019-06-17 16:40:25 +0800

    +

--- platforms/android/app/src/main/assets/www/js/moudles/detail/components/living.js
+++ platforms/android/app/src/main/assets/www/js/moudles/detail/components/living.js
@@ -1,11 +1,31 @@
-define(["lib/text!./living.html"],function(view){
+define(["lib/text!./living.html","api/api"],function(view,{getLiving}){
 	var methods = {
 	   back(){
 		   this.$router.back();
-	   }
+	   },
+	   setData(){
+			  let param = {
+				userId:this.$route.query.userId
+			  }
+			  getLiving(param).then(res=>{
+				  console.log(res);
+				  if (res.data.data)
+					  this.living = res.data.data;
+				  else
+					  this.living = "";
+			  });
+		}
 	};
 	return Vue.component("living",{
+		  data() {
+		    return {
+		      living:"",
+		    };
+		  },
 		  template:view,
-		  methods: methods
+		  methods: methods,
+		  beforeRouteEnter(to,from,next){ 
+		      next(vm=>vm.setData())
+		  },
 	})
 });
--- platforms/android/app/src/main/assets/www/js/moudles/detail/components/psy.js
+++ platforms/android/app/src/main/assets/www/js/moudles/detail/components/psy.js
@@ -1,11 +1,31 @@
-define(["lib/text!./psy.html"],function(view){
+define(["lib/text!./psy.html","api/api"],function(view,{getPsy}){
 	var methods = {
 	   back(){
 		   this.$router.back();
+	   },
+	   setData(){
+			  let param = {
+				userId:this.$route.query.userId
+			  }
+			  getPsy(param).then(res=>{
+				  console.log(res);
+				  if (res.data.data)
+					  this.psy = res.data.data;
+				  else
+					  this.psy = "";
+			  });
 	   }
 	};
 	return Vue.component("psy",{
+		data() {
+		    return {
+		      psy:"",
+		    };
+		  },
 		  template:view,
-		  methods: methods
+		  methods: methods,
+		  beforeRouteEnter(to,from,next){ 
+		      next(vm=>vm.setData())
+		  },
 	})
 });
