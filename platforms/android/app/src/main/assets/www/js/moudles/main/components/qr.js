255115c3bb9ab4455654525d228c799d814de60b
jiemaoxue <jiemx2008@163.com>
2019-06-19 15:44:15 +0800

    http

--- platforms/android/app/src/main/assets/www/js/moudles/main/components/qr.js
+++ platforms/android/app/src/main/assets/www/js/moudles/main/components/qr.js
@@ -6,21 +6,34 @@ define(["lib/text!./qr.html"],function(view){
 	   setData(){
 		   var qrUrl = this.$route.query.qr;
 		   var that = this;
-		   if (cordovaHTTP)
+		   var qrCodeImage = localStorage.getItem(qrUrl);
+		   if (qrCodeImage)
 		   {
-			   cordovaHTTP.get("https://cli.im/api/qrcode/code?", {
-				    text: qrUrl,
-				    mhid: "5hOSCA3nk88hMHcoI91VMa4"
-			   }, { }, function(response) {
-				   var html = response.data;
-				   var head = html.substring(html.lastIndexOf("<head>"),html.lastIndexOf("</head>"))
-				   var script = head.substring(head.lastIndexOf("<script>"),head.lastIndexOf("</script>"))
-				   var str = script.substring(script.lastIndexOf("qrcode_plugins_img"))
-				   var img = "https:"+str.substring(str.indexOf("\"")+1,str.lastIndexOf("\""))
-				   that.qr = img;
-			   }, function(response) {
-				   console.error(response.error);
-			   });
+			   that.qr = qrCodeImage;
+		   }else{
+			   if (window.cordovaHTTP)
+			   {
+				   window.cordovaHTTP.get("https://cli.im/api/qrcode/code?", {
+					    text: qrUrl,
+					    mhid: "5hOSCA3nk88hMHcoI91VMa4"
+				   }, { }, function(response) {
+					   var html = response.data;
+					   var head = html.substring(html.lastIndexOf("<head>"),html.lastIndexOf("</head>"))
+					   var script = head.substring(head.lastIndexOf("<script>"),head.lastIndexOf("</script>"))
+					   var str = script.substring(script.lastIndexOf("qrcode_plugins_img"))
+					   var img = "https:"+str.substring(str.indexOf("\"")+1,str.lastIndexOf("\""))
+					   
+					   window.cordovaHTTP.get(img, {}, {"Accept":"base64"}, function(response) {
+						  that.qr = "data:image/png;base64,"+response.data;
+						  localStorage.setItem(qrUrl,that.qr);
+					   }, function(response) {
+						   console.error(response.error);
+					   });
+					   //that.qr = img;
+				   }, function(response) {
+					   console.error(response.error);
+				   });
+			   }
 		   }
 	   }
 	};
--- platforms/android/app/src/main/java/com/synconset/CordovaHttpGet.java
+++ platforms/android/app/src/main/java/com/synconset/CordovaHttpGet.java
@@ -3,27 +3,17 @@
  */
 package com.synconset;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.UnknownHostException;
-import java.util.Map;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.HostnameVerifier;
-
-import javax.net.ssl.SSLHandshakeException;
+import com.github.kevinsawicki.http.HttpRequest;
+import com.github.kevinsawicki.http.HttpRequest.HttpRequestException;
 
 import org.apache.cordova.CallbackContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.util.Log;
+import java.net.UnknownHostException;
+import java.util.Map;
 
-import com.github.kevinsawicki.http.HttpRequest;
-import com.github.kevinsawicki.http.HttpRequest.HttpRequestException;
+import javax.net.ssl.SSLHandshakeException;
  
 public class CordovaHttpGet extends CordovaHttp implements Runnable {
     public CordovaHttpGet(String urlString, Map<?, ?> params, Map<String, String> headers, CallbackContext callbackContext) {
@@ -33,12 +23,17 @@ public class CordovaHttpGet extends CordovaHttp implements Runnable {
     @Override
     public void run() {
         try {
+            boolean base64 = this.getHeaders().get("Accept")==null?false:this.getHeaders().get("Accept").equalsIgnoreCase("base64");
             HttpRequest request = HttpRequest.get(this.getUrlString(), this.getParams(), false);
             this.setupSecurity(request);
             request.acceptCharset(CHARSET);
             request.headers(this.getHeaders());
             int code = request.code();
-            String body = request.body(CHARSET);
+            String body = "";
+            if (base64)
+                body = HttpRequest.Base64.encodeBytes(request.bytes());
+            else
+                body = request.body(CHARSET);
             JSONObject response = new JSONObject();
             this.addResponseHeaders(request, response);
             response.put("status", code);
