f7478523b65095ff7e9bc8ff3b9f4bcd76bb53cd
jiemaoxue <jiemx2008@163.com>
2019-07-15 11:35:10 +0800

    +

--- www/js/moudles/detail/components/living.js
+++ www/js/moudles/detail/components/living.js
@@ -1,4 +1,4 @@
-define(["lib/text!./living.html","api/api"],function(view,{getLiving,getLivingDate,getLivingId}){
+define(["lib/text!./living.html","lib/echarts.min","api/api"],function(view,echarts,{getLiving,getLivingDate,getLivingId}){
 	var methods = {
 	   back(){
 		   this.$router.back();
@@ -20,12 +20,184 @@ define(["lib/text!./living.html","api/api"],function(view,{getLiving,getLivingDa
 				  if (res.data.data)
 				  {
 					  this.dates = res.data.data;
+					  var dateArray = res.data.data.map(function(val){
+						  return val.dateStr;
+					  }).reverse();
+					  var breakfast = res.data.data.map(function(val){
+						  return val.breakfast;
+					  }).reverse();
+					  var extraMeal1 = res.data.data.map(function(val){
+						  return val.extraMeal1;
+					  }).reverse();
+					  var lunch = res.data.data.map(function(val){
+						  return val.lunch;
+					  }).reverse();
+					  var extraMeal2 = res.data.data.map(function(val){
+						  return val.extraMeal2;
+					  }).reverse();
+					  var dinner = res.data.data.map(function(val){
+						  return val.dinner;
+					  }).reverse();
+					  var nightingale = res.data.data.map(function(val){
+						  return val.nightingale;
+					  }).reverse();
+					  
+					  var violentDaysPerWeek = res.data.data.map(function(val){
+						  return val.violentDaysPerWeek;
+					  }).reverse();
+					  
+					  var moderateDaysPerWeek = res.data.data.map(function(val){
+						  return val.moderateDaysPerWeek;
+					  }).reverse();
+					  
+					  var walkDaysPerWeek = res.data.data.map(function(val){
+						  return val.walkDaysPerWeek;
+					  }).reverse();
+					  
+					  var sittingDaysRecent7Days = res.data.data.map(function(val){
+						  return val.sittingDaysRecent7Days;
+					  }).reverse();
+					  
+					  
+					  var mealChart = echarts.init(document.getElementById('meal'));
+					  var mealOption = {
+							    title: {
+							        text: '饮食\n'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    legend: {
+							        data:['','','早餐','上午加餐','午餐','下午加餐','晚餐','夜宵']
+							    },
+							    grid: {
+							    	top: '35%',
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'category',
+							        boundaryGap: false,
+							        axisLabel: {
+							            formatter: function(val){
+							            	if (val==1)
+							            		return '每天';
+							            	else if (val==2)
+							            		return '经常';
+							            	else if (val==3)
+							            		return '有时';
+							            	else if (val==4)
+							            		return '偶尔';
+							            	else if (val==5)
+							            		return '从不';
+							            }
+							        },
+							        data: ['1','2','3','4','5']
+							    },
+							    series: [
+							        {
+							            name:'早餐',
+							            type:'line',
+							            data:breakfast
+							        },
+							        {
+							            name:'上午加餐',
+							            type:'line',
+							            data:extraMeal1
+							        },
+							        {
+							            name:'午餐',
+							            type:'line',
+							            data:lunch
+							        },
+							        {
+							            name:'下午加餐',
+							            type:'line',
+							            data:extraMeal2
+							        },
+							        {
+							            name:'晚餐',
+							            type:'line',
+							            data:dinner
+							        },
+							        {
+							            name:'夜宵',
+							            type:'line',
+							            data:nightingale
+							        }
+							    ]
+							};
+					  mealChart.setOption(mealOption);
+					  
+					  
+					  var movementChart = echarts.init(document.getElementById('movement'));
+					  var movementOption = {
+							    title: {
+							        text: '运动\n'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    legend: {
+							        data:['','','剧烈运动','适度运动','步行','静坐']
+							    },
+							    grid: {
+							    	top: '35%',
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'value',
+							    	axisLabel: {
+							            formatter:'{value} 天/周'
+							        }
+							    },
+							    series: [
+							        {
+							            name:'剧烈运动',
+							            type:'line',
+							            data:violentDaysPerWeek
+							        },
+							        {
+							            name:'适度运动',
+							            type:'line',
+							            data:moderateDaysPerWeek
+							        },
+							        {
+							            name:'步行',
+							            type:'line',
+							            data:walkDaysPerWeek
+							        },
+							        {
+							            name:'静坐',
+							            type:'line',
+							            data:sittingDaysRecent7Days
+							        }
+							    ]
+							};
+					  movementChart.setOption(movementOption);
 				  }
 			  });
 		},
 		getLiving(id){
 			if (id)
 			{
+				this.charts = false;
+				this.living = "";
 				let param = {
 						id:id
 				}
@@ -37,7 +209,7 @@ define(["lib/text!./living.html","api/api"],function(view,{getLiving,getLivingDa
 				});
 			}else
 			{
-				this.living = "";
+				this.charts = true;
 			}
 		},
 	};
@@ -46,7 +218,13 @@ define(["lib/text!./living.html","api/api"],function(view,{getLiving,getLivingDa
 		    return {
 		      living:"",
 		      dates:[],
+		      charts:true,
 		    };
+		  },
+		  computed: {
+			  style() {
+		        return this.charts?"display:block;margin-top:15px":"display:none;margin-top:15px";
+		      }
 		  },
 		  template:view,
 		  methods: methods,
--- www/js/moudles/detail/components/phy.js
+++ www/js/moudles/detail/components/phy.js
@@ -1,4 +1,4 @@
-define(["lib/text!./phy.html","api/api"],function(view,{getPhy,getPhyDate,getPhyId}){
+define(["lib/text!./phy.html","lib/echarts.min","api/api"],function(view,echarts,{getPhy,getPhyDate,getPhyId}){
 	var methods = {
 	   back(){
 		   this.$router.back();
@@ -20,23 +20,262 @@ define(["lib/text!./phy.html","api/api"],function(view,{getPhy,getPhyDate,getPhy
 				  if (res.data.data)
 				  {
 					  this.dates = res.data.data;
+					  var dateArray = res.data.data.map(function(val){
+						  return val.dateStr;
+					  }).reverse();
+					  var heights = res.data.data.map(function(val){return val.height}).reverse()
+					  var heightChart = echarts.init(document.getElementById('height'));
+					  var heightOption = {
+							    title: {
+							        text: '身高'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    grid: {
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'value',
+							    	axisLabel: {
+							            formatter:'{value} cm'
+							        },
+							    },
+							    series: [
+							        {
+							            name:'身高',
+							            type:'line',
+							            data:heights
+							        }
+							    ]
+							};
+					  heightChart.setOption(heightOption);
+					  
+					  var weights = res.data.data.map(function(val){return val.weight}).reverse()
+					  var weightChart = echarts.init(document.getElementById('weight'));
+					  var weightOption = {
+							    title: {
+							        text: '体重'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    grid: {
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'value',
+							    	axisLabel: {
+							            formatter:'{value} kg'
+							        },
+							    },
+							    series: [
+							        {
+							            name:'体重',
+							            type:'line',
+							            data:weights
+							        }
+							    ]
+							};
+					  weightChart.setOption(weightOption);
+					  
+					  var bloodPressureVal5 = res.data.data.map(function(val){return val.bloodPressureVal5}).reverse()
+					  var bloodPressureVal6 = res.data.data.map(function(val){return val.bloodPressureVal6}).reverse()
+					  
+					  var bloodPressureChart = echarts.init(document.getElementById('bloodPressure'));
+					  var bloodPressureOption = {
+							    title: {
+							        text: '血压\n'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    legend: {
+							        data:['低压','高压']
+							    },
+							    grid: {
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'value',
+							        axisLabel: {
+							            formatter:'{value} mmHg'
+							        }
+							          
+							    },
+							    series: [
+							        {
+							            name:'低压',
+							            type:'line',
+							            data:bloodPressureVal5
+							        },
+							        {
+							            name:'高压',
+							            type:'line',
+							            data:bloodPressureVal6
+							        }
+							    ]
+							};
+					  bloodPressureChart.setOption(bloodPressureOption);
+					  
+					  
+					  var bloodSugarVal1 = res.data.data.map(function(val){return val.bloodSugarVal1}).reverse()
+					  var bloodSugarVal3 = res.data.data.map(function(val){return val.bloodSugarVal3}).reverse()
+					  var bloodSugarVal4 = res.data.data.map(function(val){return val.bloodSugarVal4}).reverse()
+					  
+					  
+					  var bloodSugarChart = echarts.init(document.getElementById('bloodSugar'));
+					  var bloodSugarOption = {
+							    title: {
+							        text: '血糖\n'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    legend: {
+							        data:['','空腹血糖','餐后两小时','随机']
+							    },
+							    grid: {
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'value',
+							        axisLabel: {
+							            formatter:'{value} mmol/L'
+							        }
+							          
+							    },
+							    series: [
+							        {
+							            name:'空腹血糖',
+							            type:'line',
+							            data:bloodSugarVal1
+							        },
+							        {
+							            name:'餐后两小时',
+							            type:'line',
+							            data:bloodSugarVal3
+							        },
+							        {
+							            name:'随机',
+							            type:'line',
+							            data:bloodSugarVal4
+							        }
+							    ]
+							};
+					  bloodSugarChart.setOption(bloodSugarOption);
+					  
+					  var bloodLipidVal1 = res.data.data.map(function(val){return val.bloodLipidVal1}).reverse()
+					  var bloodLipidVal2 = res.data.data.map(function(val){return val.bloodLipidVal2}).reverse()
+					  var bloodLipidVal3 = res.data.data.map(function(val){return val.bloodLipidVal3}).reverse()
+					  var bloodLipidVal4 = res.data.data.map(function(val){return val.bloodLipidVal4}).reverse()
+					  
+					  
+					  var bloodLipidChart = echarts.init(document.getElementById('bloodLipid'));
+					  var bloodLipidOption = {
+							    title: {
+							        text: '血脂\n'
+							    },
+							    tooltip: {
+							        trigger: 'axis'
+							    },
+							    legend: {
+							        data:['','','总胆固醇','甘油三酯','高密度脂蛋白胆固醇','低密度脂蛋白胆固醇']
+							    },
+							    grid: {
+							    	top: '45%',
+							        left: '3%',
+							        right: '4%',
+							        bottom: '3%',
+							        containLabel: true
+							    },
+							    xAxis: {
+							        type: 'category',
+							        boundaryGap: false,
+							        data: dateArray,
+							    },
+							    yAxis: {
+							    	type: 'value',
+							        axisLabel: {
+							            formatter:'{value} mmol/L'
+							        }
+							          
+							    },
+							    series: [
+							        {
+							            name:'总胆固醇',
+							            type:'line',
+							            data:bloodLipidVal1
+							        },
+							        {
+							            name:'甘油三酯',
+							            type:'line',
+							            data:bloodLipidVal2
+							        },
+							        {
+							            name:'高密度脂蛋白胆固醇',
+							            type:'line',
+							            data:bloodLipidVal3
+							        },
+							        {
+							            name:'低密度脂蛋白胆固醇',
+							            type:'line',
+							            data:bloodLipidVal4
+							        }
+							    ]
+							};
+					  bloodLipidChart.setOption(bloodLipidOption);
 				  }
 			  });
 	   },
 	   getPhy(id){
 		   if (id){
-			let param = {
-				id:id
-			}
-			getPhyId(param).then(res=>{
-				  if (res.data.data)
-					  this.phy = res.data.data;
-				  else
-					  this.phy = "";
-			});
+			    this.charts = false;
+			    this.phy = "";
+				let param = {
+					id:id
+				}
+				getPhyId(param).then(res=>{
+					  if (res.data.data)
+						  this.phy = res.data.data;
+					  else
+						  this.phy = "";
+				});
 		   }else
 		   {
-			   this.phy = "";
+			   this.charts = true;
 		   }
 		},
 	};
@@ -45,7 +284,13 @@ define(["lib/text!./phy.html","api/api"],function(view,{getPhy,getPhyDate,getPhy
 		    return {
 		      phy:"",
 		      dates:[],
+		      charts:true,
 		    };
+		  },
+		  computed: {
+			  style() {
+		        return this.charts?"display:block;margin-top:15px":"display:none;margin-top:15px";
+		      }
 		  },
 		  template:view,
 		  methods: methods,
